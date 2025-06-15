a28025cd9dfaeefdba2c09c6c78e73a6a9db0e79
angluisb <luis042k@hotmail.com>
2025-06-15 13:06:58 -0700

    Mejorando el ui

--- contract_ai/settings.py
+++ contract_ai/settings.py
@@ -132,11 +132,14 @@ LOGIN_URL = '/login/'                      # Redirección si no está autenticad
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 GOOGLE_AI_API_KEY = os.getenv("GOOGLE_AI_API_KEY")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
